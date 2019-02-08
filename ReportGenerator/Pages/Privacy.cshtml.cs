ee92885f60e5336b6f83af0447d645b897333064
Oleh Vintoniak <oleh.vintoniak@advancedmd.com>
2019-02-08 11:44:16 +0200

    Add project files.

---
+++ ReportGenerator/Pages/Privacy.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PDF_Generator.Utility;
+using System.IO;
+
+namespace ReportGenerator.Pages {
+  public class PrivacyModel : PageModel {
+
+    private IConverter _converter;
+
+    public PrivacyModel(IConverter converter) {
+      _converter = converter;
+    }
+
+    public IActionResult OnGet() {
+
+      var globalSettings = new GlobalSettings {
+        ColorMode = ColorMode.Color,
+        Orientation = Orientation.Portrait,
+        PaperSize = PaperKind.A4,
+        Margins = new MarginSettings { Top = 10 },
+        DocumentTitle = "PDF Report",
+        //Out = @"D:\PDFCreator\Employee_Report.pdf"  USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION
+      };
+
+      var objectSettings = new ObjectSettings {
+        PagesCount = true,
+        HtmlContent = TemplateGenerator.GetHTMLString(),
+        //Page = "https://code-maze.com/", USE THIS PROPERTY TO GENERATE PDF CONTENT FROM AN HTML PAGE
+        WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/css/SampleReport", "SampleReport.css") },
+        HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+        FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+      };
+
+      var pdf = new HtmlToPdfDocument() {
+        GlobalSettings = globalSettings,
+        Objects = { objectSettings }
+      };
+
+      var file = _converter.Convert(pdf);
+
+      return File(file, "application/pdf");
+    }
+  }
+}
---
+++ ReportGenerator/Utility/CustomAssemblyLoadContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ReportGenerator.Utility {
+  internal class CustomAssemblyLoadContext : AssemblyLoadContext {
+    public IntPtr LoadUnmanagedLibrary(string absolutePath) {
+      return LoadUnmanagedDll(absolutePath);
+    }
+    protected override IntPtr LoadUnmanagedDll(String unmanagedDllName) {
+      return LoadUnmanagedDllFromPath(unmanagedDllName);
+    }
+
+    protected override Assembly Load(AssemblyName assemblyName) {
+      throw new NotImplementedException();
+    }
+  }
+}
