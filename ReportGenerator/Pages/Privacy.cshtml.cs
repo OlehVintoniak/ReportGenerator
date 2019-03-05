88acee628b5703ec79dac7dc23a9d8edacde9001
Oleg Vintoniak <mr.mr.oleh.20@gmail.com>
2019-03-05 12:00:27 -0800

    Added study template.

--- ReportGenerator/Pages/Privacy.cshtml.cs
+++ ReportGenerator/Pages/Privacy.cshtml.cs
@@ -2,11 +2,12 @@
 using DinkToPdf.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using PDF_Generator.Utility;
+using ReportGenerator.Utility;
 using System.IO;
 
-namespace ReportGenerator.Pages {
-  public class PrivacyModel : PageModel {
+namespace ReportGenerator.Pages
+{
+    public class PrivacyModel : PageModel {
 
     private IConverter _converter;
 
@@ -16,22 +17,24 @@ namespace ReportGenerator.Pages {
 
     public IActionResult OnGet() {
 
+            
+
       var globalSettings = new GlobalSettings {
         ColorMode = ColorMode.Color,
         Orientation = Orientation.Portrait,
         PaperSize = PaperKind.A4,
-        Margins = new MarginSettings { Top = 10 },
+        //Margins = new MarginSettings { Top = 10 },
         DocumentTitle = "PDF Report",
         //Out = @"D:\PDFCreator\Employee_Report.pdf"  USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION
       };
 
       var objectSettings = new ObjectSettings {
         PagesCount = true,
-        HtmlContent = TemplateGenerator.GetHTMLString(),
+        HtmlContent = StudyReportGenerator.GetHtmlString(),//TemplateGenerator.GetHTMLString(),
         //Page = "https://code-maze.com/", USE THIS PROPERTY TO GENERATE PDF CONTENT FROM AN HTML PAGE
-        WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/css/SampleReport", "SampleReport.css") },
-        HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
-        FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+        WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Reports/StudyReport", "styles.css") },
+        //HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+        //FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
       };
 
       var pdf = new HtmlToPdfDocument() {
---
+++ ReportGenerator/Utility/StudyReportGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace ReportGenerator.Utility
+{
+    public static class StudyReportGenerator
+    {
+        public static string GetHtmlString()
+        {
+            var html = File.ReadAllText("wwwroot/Reports/StudyReport/pdf.html");
+            return html;
+        }
+    }
+}
